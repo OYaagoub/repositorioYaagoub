f2d64ef4addc8b3f5ae37e79a76ac9c2c62d4db5
OYaagoub <oyaagoub@sescam.jccm.es>
2024-06-03 01:15:24 +0200

    MS-T add Loading.... efect

--- misanuncios-ui/src/app/app.component.ts
+++ misanuncios-ui/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { FooterComponent } from './modules/includes/footer/footer.component';
 import { NavbarComponent } from './modules/includes/navbar/navbar.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,NavbarComponent],
+  imports: [RouterOutlet,FooterComponent,NavbarComponent,NgxSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- misanuncios-ui/src/app/app.config.ts
+++ misanuncios-ui/src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { Interceptor } from './infrastructure/adapters/interceptor.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 
 
@@ -12,7 +14,9 @@ import { Interceptor } from './infrastructure/adapters/interceptor.interceptor';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(),provideHttpClient(withFetch(), withInterceptors([Interceptor]))
-  ]
+  providers: [provideRouter(routes), provideClientHydration(),provideHttpClient(withFetch(), withInterceptors([Interceptor])),
+  importProvidersFrom([BrowserAnimationsModule]),provideToastr()
+  ],
+
   //{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
 };
