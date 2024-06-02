c8ecc64cc41d2ef2bdd0fa823e8c6f8290c22f02
OYaagoub <oyaagoub@sescam.jccm.es>
2024-06-03 01:24:39 +0200

    MS-T fix section card

--- misanuncios-ui/src/app/modules/products/components/card-product/card.component.ts
+++ misanuncios-ui/src/app/modules/products/components/card-product/card.component.ts
@@ -4,6 +4,7 @@ import { Product } from '../../../../domain/model/product.model';
 import { ImageService } from '../../../../application/services/image.service';
 import { ImageRepository } from '../../../../application/repositories/image.repository';
 import { ImageRepositoryImpl } from '../../../../infrastructure/repositories/image.repository.impl';
+import { ProductService } from '../../../../application/services/product.service';
 
 @Component({
   selector: 'app-card',
@@ -11,17 +12,13 @@ import { ImageRepositoryImpl } from '../../../../infrastructure/repositories/ima
   imports: [RouterLink],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
-  providers: [ImageService,
-    {
-      provide: ImageRepository,
-      useClass: ImageRepositoryImpl,
-    }],
+
 })
 export class CardComponent {
   @Input() product!: Product;
 
 
-  constructor(private imageService: ImageService) {
+  constructor() {
 
   }
 
