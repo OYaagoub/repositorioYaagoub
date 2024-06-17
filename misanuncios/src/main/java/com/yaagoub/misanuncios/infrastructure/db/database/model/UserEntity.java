fd347b924e4e427fb5fac327b93b4ff078b33d71
OUSSAMA YAAGOUB <oyaagoub@sescam.jccm.es>
2024-06-17 12:04:07 +0200

    MS-S Fix User table

--- misanuncios/src/main/java/com/yaagoub/misanuncios/infrastructure/db/database/model/UserEntity.java
+++ misanuncios/src/main/java/com/yaagoub/misanuncios/infrastructure/db/database/model/UserEntity.java
@@ -16,7 +16,7 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  long id;
     private  String name;
-
+    @Column(unique=true)
     private String image;
 
     private String email;
